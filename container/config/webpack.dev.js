aa3832ba72751e9bdbf522f41ab6c806ff17e78f
Mohy Elden, Yasmeen <ymohyelden@deloitte.com>
2025-01-16 18:32:40 +0200

    set up the project

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const packageJson = require("../package.json")
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: { 
+        index: "index.html"
+     },
+  },
+  plugins :[
+    new ModuleFederationPlugin({
+      name:'container', //optionally it will not used as it is the host one but useful for documentation
+      shared:packageJson.dependencies,
+      remotes:{
+        'marketing':'marketing@http://localhost:8081/remoteEntry.js'
+      }
+    }),
+    new HtmlWebpackPlugin({
+        template: './public/index.html'
+    })
+  ]
+};
+module.exports = merge(commonConfig, devConfig);
---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Marketing from './components/Marketing'
+
+
+export default ()=>{
+    return <div> <h1>
+        Hello Hello
+        <hr></hr>
+        <Marketing/>
+    </h1> 
+    </div>;
+}
